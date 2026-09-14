9e3e98c1e119e4f4fab0a39a51fbda5c65df0d7f
Toumesis <tomas.kuna.s@gyarab.cz>
2026-09-14 10:02:13 +0200

    Nekompletni programn ktery hleda nejcastejsi cislo

---
+++ src/main/java/cz/gyarab/Tomas/Cislo.java
@@ -0,0 +1,23 @@
+package cz.gyarab.Tomas;
+
+import java.util.Scanner;
+
+public class Cislo {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        int[] cisla = new int[10];
+
+        //nacteni cisel ze skeneru
+        for (int i = 0; i < cisla.length; i++){
+            cisla[i] = scanner.nextInt();
+        }
+
+        int nejcastejsiCislo = 0;
+
+        //hledani cisla nejdcastejsihio
+        for (int j = 0; j < cisla.length; j++) {
+            int pocet = 0;
+
+        }
+    }
+}
