00bf2f96c60e20d0e7d623a752dac4f28f5a7580
A-Y-S-01 <ali.yunussov.s@gyarab.cz>
2026-09-14 09:04:26 +0200

    Mode (Ali)

---
+++ src/main/java/cz/gyarab/ali/Mode.java
@@ -0,0 +1,40 @@
+package cz.gyarab.ali;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+public class Mode {
+    static void main() {
+        var numbers = new ArrayList<Integer>();
+        var random = new Random();
+
+        for (int i = 0; i < 10; i++)
+            numbers.add(random.nextInt(101));
+
+        IO.println(numbers);
+
+        getMode(numbers);
+    }
+
+    static void getMode(List<Integer> arr) {
+        var counts = new HashMap<Integer, Integer>();
+
+        var mode = 0;
+        var modeCount = 0;
+
+        for (int x : arr) {
+            var count = counts.getOrDefault(x, 0) + 1;
+            counts.put(x, count);
+
+            if (count > modeCount) {
+                mode = x;
+                modeCount = count;
+            }
+        }
+
+        IO.println(counts);
+        IO.println("mode: " + mode + " (" + modeCount + "x)");
+    }
+}
