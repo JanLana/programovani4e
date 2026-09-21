9ff1bee71a35ec85dd1bfb12ad0507897b26b364
Toumesis <tomas.kuna.s@gyarab.cz>
2026-09-21 07:50:49 +0200

    Program ktery hleda nejcastejsi cislo

---
+++ src/main/java/cz/gyarab/Tomas/NejcastejsiCislo.java
@@ -0,0 +1,31 @@
+package cz.gyarab.Tomas;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class NejcastejsiCislo {
+    public static void main(String[] args) {
+        ArrayList<Integer> cisla = new ArrayList<>();
+        cisla.add(5);
+        cisla.add(6);
+        cisla.add(6);
+        cisla.add(6);
+        cisla.add(5);
+        cisla.add(6);
+
+        int nejcastejsiCislo = cisla.get(0);
+        int maxPocet = 0;
+
+        for (int cislo : cisla) {
+            int pocet = Collections.frequency(cisla, cislo);
+
+            if (pocet > maxPocet) {
+                maxPocet = pocet;
+                nejcastejsiCislo = cislo;
+            }
+        }
+
+        System.out.println(nejcastejsiCislo);
+
+    }
+}
